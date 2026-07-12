dce1f40f8e6d08fae144102986cceeeb106a0e1c
xbale047 <iqbalpamungkas0927@gmail.com>
2026-07-13 02:44:39 +0700

    Update sc.js

--- sc.js
+++ sc.js
@@ -1,28 +1,11 @@
-// Sembunyikan halaman utama saat awal dibuka
-document.getElementById("main").style.display = "none";
-
-
-// Fungsi tombol Mulai Sekarang
-function masukWebsite() {
+function openSerifest(){
 
     const welcome = document.getElementById("welcome");
     const main = document.getElementById("main");
 
-    // Hilangkan welcome
-    welcome.style.opacity = "0";
 
-    setTimeout(() => {
+    welcome.style.display="none";
 
-        welcome.style.display = "none";
-
-        // Tampilkan halaman utama
-        main.style.display = "block";
-        main.style.opacity = "0";
-
-        setTimeout(() => {
-            main.style.opacity = "1";
-        }, 50);
-
-    }, 500);
+    main.style.display="block";
 
 }
