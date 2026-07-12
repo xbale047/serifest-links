10c5568d0c0a00d926c9e9d86a1f463aa31ae616
xbale047 <iqbalpamungkas0927@gmail.com>
2026-07-13 02:41:11 +0700

    Update sc.js

--- sc.js
+++ sc.js
@@ -1,11 +1,28 @@
-const tombol = document.querySelectorAll(".btn");
+// Sembunyikan halaman utama saat awal dibuka
+document.getElementById("main").style.display = "none";
 
-tombol.forEach(btn => {
 
-btn.addEventListener("click", () => {
+// Fungsi tombol Mulai Sekarang
+function masukWebsite() {
 
-console.log(btn.innerText + " diklik");
+    const welcome = document.getElementById("welcome");
+    const main = document.getElementById("main");
 
-});
+    // Hilangkan welcome
+    welcome.style.opacity = "0";
 
-});
+    setTimeout(() => {
+
+        welcome.style.display = "none";
+
+        // Tampilkan halaman utama
+        main.style.display = "block";
+        main.style.opacity = "0";
+
+        setTimeout(() => {
+            main.style.opacity = "1";
+        }, 50);
+
+    }, 500);
+
+}
